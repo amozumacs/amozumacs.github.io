4c2a134ce1815470a9e809ea60aeab464e296f80
amozumacs <amozumacs>
2024-05-18 00:29:15 +0300

    Add minor style fixes

--- script.js
+++ script.js
@@ -6,21 +6,17 @@ function darkmode() {
 
     document.querySelector('.header-block').classList.toggle('dark-header-block', !wasDarkmode);
     document.querySelector('.footer').classList.toggle('dark-footer-block', !wasDarkmode);
-    document.querySelectorAll('.first-block').forEach(el => el.classList.toggle('dark-first-block', !wasDarkmode));
-    document.querySelectorAll('.second-block').forEach(el => el.classList.toggle('dark-second-block', !wasDarkmode));
+    document.querySelectorAll('.content-block').forEach(el => el.classList.toggle('dark-content-block', !wasDarkmode));
 }
-
 
 function onload() {
     const isDarkmode = localStorage.getItem('darkmode') === 'true';
     document.body.classList.toggle('dark-mode', isDarkmode);
 
-    // Toggle dark mode classes for specific elements
     document.querySelector('.header-block').classList.toggle('dark-header-block', isDarkmode);
     document.querySelector('.footer').classList.toggle('dark-footer-block', isDarkmode);
-
-    document.querySelectorAll('.first-block').forEach(el => el.classList.toggle('dark-first-block', isDarkmode));
-    document.querySelectorAll('.second-block').forEach(el => el.classList.toggle('dark-second-block', isDarkmode));
+    document.querySelectorAll('.content-block').forEach(el => el.classList.toggle('dark-content-block', isDarkmode));
 }
 
 window.onload = onload;
+
