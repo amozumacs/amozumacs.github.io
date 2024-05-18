b48192fce4880909e6b8130f7c04d796bd24ce8d
amozumacs <amozumacs>
2024-05-18 13:03:12 +0300

    Fix about-me section dark mode and add more primary and secondary blocks to dark mode

--- script.js
+++ script.js
@@ -8,7 +8,18 @@ function toggleDarkMode(isDarkmode) {
     document.body.classList.toggle('dark-mode', isDarkmode);
     document.querySelector('.header-block').classList.toggle('dark-header-block', isDarkmode);
     document.querySelector('.footer').classList.toggle('dark-footer-block', isDarkmode);
-    document.querySelectorAll('.content-block').forEach(el => el.classList.toggle('dark-content-block', isDarkmode));
+    document.querySelectorAll('.content-block').forEach(el => {
+        el.classList.toggle('dark-content-block', isDarkmode);
+        if (el.classList.contains('first-block')) {
+            el.classList.toggle('dark-first-block', isDarkmode);
+        }
+        if (el.classList.contains('second-block')) {
+            el.classList.toggle('dark-second-block', isDarkmode);
+        }
+        if (el.classList.contains('about-block')) {
+            el.classList.toggle('dark-about-block', isDarkmode);
+        }
+    });
 }
 
 function onload() {
